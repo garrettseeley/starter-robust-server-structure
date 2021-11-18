d2b23ef0e4610fe716832a93f80b53bdac15fa65
Garrett <seeleysurfer01@gmail.com>
2021-11-18 13:58:55 -0600

    Added Error Types and Organized

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const app = express();
+const counts = require("./data/counts-data");
+
+const flipsRouter = require("./flips/flips.router")
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+
+app.use(express.json());
+
+app.use("/flips", flipsRouter);
+// Middleware function to validate the request body
+
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next({status: 404, message: `Count id not found: ${countId}`});
+  } else {
+    res.json({ data: foundCount });
+  }
+});
+
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+});
 
 // Not found handler
 app.use((request, response, next) => {
@@ -9,9 +32,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({error: message});
 });
 
 module.exports = app;
